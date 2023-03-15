da76654bc89df963482a528c03e7ae5ae552a4dc
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-03-15 19:06:24 -0300

    criado repositorio novo, adiciando 2 arquivos em Ruby.

---
+++ Classes/Principal.rb
@@ -0,0 +1,9 @@
+require "./CursoDioMe/Classes/Conta_bancaria"
+
+conta_ricardo = ContaBancaria.new('ricardo', 100)
+conta_pessoa2 = ContaBancaria.new('pessoa2', 200)
+
+conta_cadu.transferir(conta_pessoa2, 50)
+
+p conta_ricardo.saldo  #-50
+p conta_pessoa2.saldo  #+250
