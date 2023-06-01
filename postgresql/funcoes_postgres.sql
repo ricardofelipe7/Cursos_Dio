1cfa13f5f59d83b71581609f8e5eafb25aea4d2b
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-06-01 17:05:38 -0300

    Doc: atualizando documentos de comandos sql e treinando comandos Joins.

--- postgresql/funcoes_postgres.sql
+++ postgresql/funcoes_postgres.sql
@@ -21,3 +21,28 @@ SELECT COUNT(numero) FROM cliente
 SELECT COUNT(numero), email FROM cliente
 WHERE email ILIKE '%gmail.com'
 GROUP BY email;
+
+SELECT MAX(numero) FROM cliente;
+SELECT MIN(numero) FROM cliente;
+SELECT MAX(valor) FROM cliente_transacoes;
+SELECT MIN(valor) FROM cliente_transacoes;
+
+SELECT MAX(valor), tipo_transacao_id FROM cliente_transacoes
+GROUP BY tipo_transacao_id;
+
+SELECT min(valor), tipo_transacao_id FROM cliente_transacoes
+GROUP BY tipo_transacao_id;
+
+SELECT COUNT(id), tipo_transacao_id FROM cliente_transacoes
+GROUP BY tipo_transacao_id
+HAVING COUNT(id) > 150;
+
+SELECT SUM(valor) FROM cliente_transacoes;
+
+SELECT SUM(valor), tipo_transacao_id FROM cliente_transacoes
+GROUP BY tipo_transacao_id
+ORDER BY tipo_transacao_id ASC;
+
+SELECT SUM(valor), tipo_transacao_id FROM cliente_transacoes
+GROUP BY tipo_transacao_id
+ORDER BY tipo_transacao_id DESC;
