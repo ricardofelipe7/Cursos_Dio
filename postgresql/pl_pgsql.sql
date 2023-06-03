6f1addb9835a08e690205f3273ac10cefa9144a5
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-06-03 17:50:02 -0300

    Feat: arquivo sql sobre funções para desenvolvedor na linguagem pgsql.

---
+++ postgresql/pl_pgsql.sql
@@ -0,0 +1,50 @@
+CREATE OR REPLACE FUNCTION func_somar(INTEGER, INTEGER)
+RETURNS INTEGER
+SECURITY DEFINER
+-- RETURNS NULL ON NULL INPUT
+CALLED ON NULL INPUT
+LANGUAGE SQL
+AS $$
+    SELECT COALESCE ($1, null) + COALESCE($2, null);
+$$;
+
+SELECT func_somar(1,100);
+
+SELECT COALESCE(null, null, 'digital', 'daniel');
+
+----------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION bancos_add(p_numero INTEGER, p_nome VARCHAR, p_ativo BOOLEAN)
+RETURNS INTEGER
+SECURITY INVOKER
+LANGUAGE PLPGSQL
+CALLED ON NULL INPUT
+AS $$
+    DECLARE variavel_id INTEGER;
+
+BEGIN
+    IF p_numero IS NULL OR p_nome IS NULL OR p_ativo IS NULL THEN
+        RETURN 0;
+    END IF;
+
+    SELECT INTO variavel_id p_numero
+    FROM banco
+    WHERE numero = p_numero;
+
+    IF variavel_id IS NULL THEN
+        INSERT INTO banco(numero, nome, ativo)
+        VALUES (p_numero, p_nome, p_ativo);
+    ELSE
+        RETURN variavel_id;
+    END IF;
+
+    SELECT INTO variavel_id numero
+    FROM banco
+    WHERE numero = p_numero;
+
+        RETURN variavel_id;
+END; $$;
+
+SELECT bancos_add(5433, 'Banco Novo em outra porta', true);
+
+SELECT numero, nome, ativo FROM banco WHERE numero = 5433;
